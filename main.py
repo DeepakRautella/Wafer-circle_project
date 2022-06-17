2760f44e5226b6a49f1cd5ad82cfa02c4b561868
DeepakRautella <deepak.rautella@gmail.com>
2022-06-17 16:50:53 +0530

    filed modified

--- main.py
+++ main.py
@@ -103,5 +103,5 @@ if __name__ == "__main__":
     host = '0.0.0.0'
     # port = 5000
     httpd = simple_server.make_server(host, port, app)
-    # print("Serving on %s %d" % (host, port))
+    print("Serving on %s %d" % (host, port))
     httpd.serve_forever()
